50f409350604296dc04f615ff381f76e9292f958
khandragon <saaadkhan23@yahoo.ca>
2018-04-28 15:14:26 -0400

    add input buttons

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 "use strict";
-
-/**var date_regex = /^(0[1-9]|1[0-2])\/(0[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
+/**
  * createDummyElements - creates sematic elements which are not supported by IE8
  *
  */
@@ -13,27 +12,30 @@ function createDummyElements() {
         document.createElement(semanticElements[i]);
     }
 }
-
 function main() {
     console.log("start");
+    //noScrollJumping();
     invisbleAll();
     selectedTop();
     U.addHandler(U.$("topArt"), "click", selectedTop);
     U.addHandler(U.$("savedArt"), "click", selectedSaved);
     U.addHandler(U.$("chartArt"), "click", selectedChart);
-    U.addHandler(U.$("submitBtn"),"click", submitData);
+    U.addHandler(U.$("submitBtn"), "click", submitData);
 }
 function selectedTop() {
     invisbleAll();
+    console.log("top");
     U.$("topContent").style.display = "block";
     U.$("topContent").style.backgroundcolor = "grey";
 
 }
 function selectedSaved() {
+    console.log("saved");
     invisbleAll();
     U.$("savedContent").style.display = "block";
 }
 function selectedChart() {
+    console.log("chart");
     invisbleAll();
     U.$("chartContent").style.display = "block";
 }
@@ -43,24 +45,26 @@ function invisbleAll() {
         allContent[i].style.display = "none";
     }
 }
-
 if (!document.addEventListener) {
     createDummyElements();
 }
-
 function dateIsValid(testDate) {
-    var date_regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[01])$/ ;
-    if(!(date_regex.test(testDate))){
+    var date_regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[01])$/;
+    if (!(date_regex.test(testDate))) {
         return false;
     }
     else
         return true;
 }
-
 function submitData() {
- var date = U.$("date").value;
- console.log(dateIsValid(date));  
- console.log(date);
-  
+    var date = U.$("date").value;
+    if (dateIsValid(date)) {
+        U.$("date").style.color="black";
+        var numArt = U.$("numArt");
+        var language = U.$("langSelect");
+    }
+    else {
+        U.$("date").style.color="red";
+    }
 }
 U.ready(main);
