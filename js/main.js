5410bdd087268cbb8aab1c96bf83f3825d4c9f09
khandragon <saaadkhan23@yahoo.ca>
2018-04-30 00:34:50 -0400

    add images appearing alongside text

--- js/main.js
+++ js/main.js
@@ -41,20 +41,54 @@ if (!document.addEventListener) {
 }
 function removeData() {
     var data = U.$("results");
-    data.innerHTML='';
+    data.innerHTML = '';
+}
+function processTitles(responseText) {
+    var pageInfo = JSON.parse(responseText);
+    var result = document.createElement("p");
+    var imgContainer = document.createElement("div");
+    var summarynode = document.createTextNode(pageInfo.extract);
+    var target = U.$(pageInfo.titles.canonical);
+    if(pageInfo.thumbnail !== undefined){
+        var img = document.createElement("img");
+        img.src=pageInfo.thumbnail.source;
+        imgContainer.appendChild(img);
+        result.appendChild(imgContainer);
+    }
+    result.appendChild(summarynode);
+    target.appendChild(result);
+}    
+
+function readTitle(url) {
+    var r = new XMLHttpRequest();
+    r.open("GET", url, true);
+    r.setRequestHeader("Api-User-Agent", "saaadkhan23@yahoo.ca");
+    console.log("set the request header");
+    U.addHandler(r, "load", function () {
+        if (r.readyState === 4) {
+            processTitles(r.responseText);
+        }
+    });
+    r.send(null);
+}
+function getExtractPictures(titles) {
+    var language = U.$("langSelect").value;
+    for (var i = 0; i < titles.length; i++) {
+        readTitle("https://" + language + ".wikipedia.org/api/rest_v1/page/summary/" + titles[i]);
+    }
 }
 function populateIndex(titles, numViews) {
     var results = U.$("results");
     for (let i = 0; i < titles.length; i++) {
-
-        var resultnode = document.createElement("p");
-        var titlenode = document.createTextNode((i+1)+": "+titles[i]);
-        var viewnode = document.createTextNode("    Number of Views: "+numViews[i])
+        var resultnode = document.createElement("div");
+        var titlenode = document.createTextNode((i + 1) + ": " + titles[i]);
+        var viewnode = document.createTextNode("\br\n   Number of Views: " + numViews[i])
         resultnode.appendChild(titlenode);
         resultnode.appendChild(viewnode);
+        resultnode.id = titles[i];
         results.appendChild(resultnode);
     }
-
+    console.log("done");
 }
 function dateIsValid(testDate) {
     var date_regex = /^(19|20)\d{2}-(0[1-9]|1[0-2])-(0[1-9]|1\d|2\d|3[01])$/;
@@ -95,9 +129,8 @@ function processText(responseText) {
     }
     topViewed = topViewed.filter(String);
     numViews = numViews.filter(String);
-    populateIndex(topViewed, numViews)
-    console.log(topViewed);
-    console.log(numViews);
+    populateIndex(topViewed, numViews);
+    getExtractPictures(topViewed);
 }
 function readFile(url, numArt) {
     console.log(url);
